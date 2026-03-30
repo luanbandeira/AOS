ff01ffbae5bdc7ddb8e54cf37237ada49a92d5e0
luanbandeira <luanbdmr@gmail.com>
2026-03-30 22:17:53 +0000

    commit

---
+++ express-01/api/models/index.js
@@ -0,0 +1,32 @@
+import Sequelize from "sequelize";
+import pg from "pg";
+
+import getUserModel from "./user";
+import getMessageModel from "./message";
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  dialectModule: pg,
+});
+
+const models = {
+  User: getUserModel(sequelize, Sequelize),
+  Message: getMessageModel(sequelize, Sequelize),
+};
+
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
